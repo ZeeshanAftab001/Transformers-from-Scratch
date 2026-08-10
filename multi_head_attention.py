72bf1858c6807d8aa37161696ceb8335a40f4e22
ZeeshanAftab001 <zeeshanaftababbasi@gmail.com>
2026-08-10 20:41:31 +0500

    Bugs Removed from MultiHead Attention Class

--- multi_head_attention.py
+++ multi_head_attention.py
@@ -13,17 +13,18 @@ class MultiHeadAttention(nn.Module):
         assert model_dim % n_heads == 0 ,"model_dim must be completely divisible by n_heads"
         self.d_k=self.model_dim // n_heads # Dimension of vector seen by each head
 
-        self.w_q=nn.Linear(self.model_dim,self.model_dim)
-        self.w_k=nn.Linear(self.model_dim,self.model_dim)
-        self.w_v=nn.Linear(self.model_dim,self.model_dim)
+        self.w_q=nn.Linear(self.model_dim,self.model_dim,bias=False)
+        self.w_k=nn.Linear(self.model_dim,self.model_dim,bias=False)
+        self.w_v=nn.Linear(self.model_dim,self.model_dim,bias=False)
 
-        self.w_o=nn.Linear(self.model_dim,self.model_dim)
+        self.w_o=nn.Linear(self.model_dim,self.model_dim,bias=False)
         self.dropout=nn.Dropout(p=self.dropout_p)
 
-    def attention(self,query,key,value,mask,dropout):
-
+    @staticmethod
+    def attention(query,key,value,mask,dropout):
+        d_k=query.shape[-1]
         # (batch, h, seq_len, d_k) --> (batch, h, seq_len, seq_len)
-        attention_scores = (query @ key.transpose(-2, -1)) / torch.sqrt(torch.tensor(self.d_k))
+        attention_scores = (query @ key.transpose(-2, -1)) / torch.sqrt(torch.tensor(d_k))
         if mask is not None:
             # Write a very low value (indicating -inf) to the positions where mask == 0
             attention_scores.masked_fill_(mask == 0, -1e9)
@@ -42,17 +43,17 @@ class MultiHeadAttention(nn.Module):
         value=self.w_v(x)
 
         # (batch_size,seq_len,model_dim) -> (batch_size,seq_len,n_heads,self.d_k) -> (batch_size,n_heads,seq_len,self.d_k) 
-        query=query.view(query.shape[0],query.view[1],self.n_heads,self,self.d_k).transpose(1,2)
-        key=key.view(query.shape[0],query.view[1],self.n_heads,self,self.d_k).transpose(1,2)
-        value=value.view(query.shape[0],query.view[1],self.n_heads,self,self.d_k).transpose(1,2)
+        query=query.view(query.shape[0],query.shape[1],self.n_heads,self.d_k).transpose(1,2)
+        key=key.view(query.shape[0],query.shape[1],self.n_heads,self.d_k).transpose(1,2)
+        value=value.view(query.shape[0],query.shape[1],self.n_heads,self.d_k).transpose(1,2)
 
 
         value,self.attention_scores=MultiHeadAttention.attention(query,key,value,mask,self.dropout)
 
         # Combine all the heads together
         # (batch, h, seq_len, d_k) --> (batch, seq_len, h, d_k) --> (batch, seq_len, d_model)
-        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+        value = value.transpose(1, 2).contiguous().view(value.shape[0], -1, self.n_heads * self.d_k)
 
          # Multiply by Wo
         # (batch, seq_len, d_model) --> (batch, seq_len, d_model)  
-        return self.w_o(x)
+        return self.w_o(value)
